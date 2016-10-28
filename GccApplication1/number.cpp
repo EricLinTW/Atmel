302e38a2672e521b08d0ec68d7b0de3b17404736
EricLinTW <lericccg@gmail.com>
2016-10-28 23:43:44 +0800

    Revert "Revert "add week 1 app""
    This reverts commit 4e8ad23f013d1c2028dac14a6bb04407a3d177a1.

---
+++ GccApplication1/number.cpp
@@ -0,0 +1,6 @@
+﻿/*
+ * number.cpp
+ *
+ * Created: 2016/10/5 上午 11:24:53
+ *  Author: NIU
+ */ 
---
+++ GccApplication1/w5_number.cpp
@@ -0,0 +1,86 @@
+﻿/*
+ * CPPFile1.cpp
+ *
+ * Created: 2016/10/12 上午 10:02:09
+ *  Author: Aran
+ */ 
+#include "w5_number.h"
+#include <avr/io.h>
+
+void delay_105(volatile long m) //subroutine
+{
+	for(volatile long k=0; k<m; k++);
+}
+void five(void)
+{
+	PORTD |= (1<<b) | (1<<e);
+	PORTD &= (~(1<<a)) & (~(1<<c)) & (~(1<<d));
+	PORTC &= (~(1<<f)) & (~(1<<g));
+}
+void zero(void)
+{
+	PORTD |= (1<<b) | (1<<e) | (~(1<<a)) | (~(1<<c)) | (~(1<<d));
+	PORTC |= (~(1<<f)) | (~(1<<g));
+}
+/*
+char add(char i, char j)
+{
+	return i+j;
+}
+*/
+int add(int i, int j)
+{
+	return i+j;
+}
+
+
+void two(void)
+{
+	PORTD &= (~(1<<a)) & (~(1<<b)) & (~(1<<d)) & (~(1<<e));
+	PORTC &= (~(1<<f)) & (~(1<<g));
+	PORTD |= (1<<c);
+	PORTC |= (1<<f);
+}
+void four(void)
+{
+	PORTD &= (~(1<<b)) & (~(1<<c));
+	PORTC &= (~(1<<f)) & (~(1<<g));
+	PORTD |= (1<<a) | (1<<d) | (1<<e);
+}
+void six(void)
+{
+	PORTD &= (~(1<<a)) & (~(1<<c))& (~(1<<d)) & (~(1<<e));
+	PORTC &= (~(1<<f)) & (~(1<<g));
+	PORTD |= (1<<b);
+}
+void eight(void)
+{
+	PORTD &= (~(1<<a)) &(~(1<<b)) & (~(1<<c)) & (~(1<<d)) & (~(1<<e));
+	PORTC &= (~(1<<f)) & (~(1<<g));
+}
+void one(void)
+{
+	PORTD &= (~(1<<b)) & (~(1<<c));
+	PORTC |= (1<<f) | (1<<g);
+	PORTD |= (1<<a) | (1<<d) | (1<<e);
+}
+void three(void)
+{
+	PORTD &= (~(1<<a)) & (~(1<<c)) & (~(1<<d)) & (~(1<<e));
+	PORTC &= (~(1<<g));
+	PORTD |= (1<<b);
+	PORTC |= (1<<f);
+}
+void seven(void)
+{
+	PORTD &= (~(1<<a)) &(~(1<<b)) & (~(1<<c)) ;
+	PORTC |= (1<<f) | (1<<g);
+	PORTD |= (1<<d) | (1<<e);
+}
+	
+void nine(void)
+{
+	PORTD &= (~(1<<a)) &(~(1<<b)) & (~(1<<c)) & (~(1<<e));
+	PORTC &= (~(1<<f)) & (~(1<<g));
+	PORTD |=(1<<d);
+}
